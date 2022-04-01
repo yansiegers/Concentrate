6c5359d7b4547b089da3b0a4b7b597124aa3d365
Yan Siegers <hello@yansiegers.nl>
2022-04-01 12:13:34 +0200

    Add stream display for slider

--- game.js
+++ game.js
@@ -1,14 +1,28 @@
+const sliderElement = document.getElementById('slider');
+sliderElement.value = 90;
+sliderElement.oninput = () => {
+  game.slider = sliderElement.value;
+  document.getElementById('amount').innerHTML = sliderElement.value;
+};
+
 const game = {
   waterLevel: 0,
+  slider: 90,
 
   onload() {
     setInterval(
       () => {
+        this.streamDisplay();
         if (this.waterLevel < 500) {
           this.fillUp();
         }
       }, 10
     );
+  },
+
+  streamDisplay() {
+    const streamElement = document.getElementById('stream');
+    streamElement.style.width = `${this.slider}px`;
   },
 
   fillUp() {
